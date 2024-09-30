0f207ac0d9cb1de3c3f75669e046613b549af406
ZhuAnson-ICD201-01 <anson.zhu@student.tdsb.on.ca>
2024-09-30 14:13:36 +0000

    asd

--- PP1_9.py
+++ PP1_9.py
@@ -1,31 +1,37 @@
 
 
 def q1(): 
-  #Write Assignment code here
+  print ('"Hello World"')
 
 
 def q2(): 
-  #Write Assignment code here
+ ohio =  input ("Input a word: ")
+ print (ohio).lower())
+ print (ohio.upper())
 
-
+ 
 def q3(): 
-  #Write Assignment code here
+  skibidi = input("Input a word that is at least 5 letters long: ") 
+  print (skibidi[1:4])
 
 
 def q4(): 
-  #Write Assignment code here
+  real = input("Input a word: ")
+  print (real.index("o"))
+
 
 
 def q5(): 
-  #Write Assignment code here
+  khirushan = input("Input a word: ")
+  print (len(khirushan))
 
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+#q1()
+#q2()
+#q3()
+#q4()
+#q5()
